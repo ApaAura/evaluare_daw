dbe0a5e8c84d261178ea6477e558e5b4b346e4d9
Apareci Aura <aurica.apareci@gmail.com>
2024-02-13 10:01:39 +0200

    Configured routes, Added CarController

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Car;
+
+class CarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $cars = Car::all();
+        return view('cars.index', ['cars'=>$cars]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('cars.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'brand' => 'required',
+            'model' => 'required',
+            'year' => 'required|numeric',
+            'color' => 'required'
+        ]);
+
+        $car = Car::create($request->all());
+
+        return redirect()->route('cars.index')->with('success', 'Car created successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Car $car)
+    {
+        return view('cars.edit',['car'=>$car]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Car $car)
+    {
+        $request->validate([
+            'brand' => 'required',
+            'model' => 'required',
+            'year' => 'required|numeric',
+            'color' => 'required'
+        ]);
+
+        $car->update($request->all());
+
+        return redirect()->route('cars.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Car $car)
+    {
+        $car->delete();
+        return redirect()->route('cars.index');
+    }
+    
+}
